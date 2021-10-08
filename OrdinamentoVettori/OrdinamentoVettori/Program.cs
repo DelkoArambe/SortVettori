b19684d0c82b24fb7b82ead2df4381868eb156a7
Andrea Marini <ANDREA.MARINI@local.ittcesena.it>
2021-10-08 08:35:17 +0200

    aggiunta commenti

--- OrdinamentoVettori/OrdinamentoVettori/Program.cs
+++ OrdinamentoVettori/OrdinamentoVettori/Program.cs
@@ -6,17 +6,21 @@ namespace OrdinamentoVettori
     {
         static void Main(string[] args)
         {
+            //creazione e riempimento del vettore di interi
             int [] vettore = new int [10] { 11, 55, -9, 7, 74, 147, -87, 6, 0, 18};
 
 
+            //chiamo il metodo per l'ordinamento
             ordina(vettore);
 
+            //stampo a video gli elementi nel vettore
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(vettore[i]);
 
             Console.ReadKey();
         }
 
+        //metodo ordina
         static void ordina (int[] vettore)
         {
             Array.Sort(vettore);
